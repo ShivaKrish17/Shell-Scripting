abb1b3d55b0013249b7890126fcab82f4eeb5419
unknown <shiva539k@gmail.com>
2024-03-16 22:18:48 +0530

    Hello World Commit

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -2,17 +2,17 @@
 
 echo "Hello World"
 
-Person1=Sam
+PERSON1=Sam
 PERSON2=Bob
 
-echo "$Person1: Oh? Bob!" 
+echo "$PERSON1: Oh? Bob!" 
 
 echo "$PERSON2: Hey Sam! Good to see you!"
 
-echo "$Person1: How’s it going?" 
+echo "$PERSON1: How’s it going?" 
 
 echo "$PERSON2: Yeah, good. Working a lot. And you?"
 
-echo "$Person1: I went back to school."
+echo "$PERSON1: I went back to school."
 
 echo "$PERSON2: Good for you!"
