f9fc78cb765aca47bdca337f1b4052999b789e7a
unknown <shiva539k@gmail.com>
2024-03-17 08:49:00 +0530

    Hello World Commit

--- 02-variables.sh
+++ 02-variables.sh
@@ -2,4 +2,4 @@
 #Shell Script run the command inside $() and stores the output into variable called DATE.
 DATE=$(date)
 
-echo "This script is executed at timestamp: $DATE"
+echo "This script is executed at timestamp: $DATE"   #sample output : This script is executed at timestamp: Sun Mar 17 03:17:35 UTC 2024
