57cce23b1e53b71a35c872f77d15d47294dfc083
unknown <shiva539k@gmail.com>
2024-03-17 09:42:52 +0530

    Prompt script

--- 05-variables.sh
+++ 05-variables.sh
@@ -8,6 +8,6 @@ echo "Useraname entered is: $USERNAME"
 
 echo "Please enter your password"
 
-read -s PASSWORD
+read -s PASSWORD    # -s : do not echo the value which is being typing in command line(or do not echo input coming from a terminal)
 
 echo "Password entered is: $PASSWORD"
