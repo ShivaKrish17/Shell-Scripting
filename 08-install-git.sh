0ea6cb4d90634dc49db9ac1e22437a36bee31f8d
unknown <shiva539k@gmail.com>
2024-03-17 12:40:23 +0530

    Validate Root User: GIT Installation Script

--- 08-install-git.sh
+++ 08-install-git.sh
@@ -7,4 +7,7 @@ USERID=$(id -u)           # id -u returns user id, root has id as 0
 if [ $USERID -ne 0 ]      # -ne: not equal, -eq -> equal operator  
 then
     echo "ERROR: Please Run Script with Root User"
+    exit 15               # 1 - 127 is failure
 fi    
+
+yum install git -y
