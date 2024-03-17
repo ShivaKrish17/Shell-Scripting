863623467f3e8496e79b99fcaf77838daada7593
unknown <shiva539k@gmail.com>
2024-03-17 09:26:43 +0530

    Addition of two numbers script

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Program: Addition of two numbers
+
+NUMBER1=$1
+NUMBER2=$2
+
+#Run command inside shell to add these 2 numbers
+
+SUM=$(NUMBER1+NUMBER2)
+
+echo "Addition of two numbers is: $SUM"
