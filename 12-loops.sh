a5e2b8572875855ec4bbf7328aaf56697233a547
unknown <shiva539k@gmail.com>
2024-03-17 15:30:18 +0530

    Package loops 1 Script

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOG_FILE=/tmp/$SCRIPT_NAME-$DATE.log
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$R Installing $2 is FAILURE $N"
+        exit 1
+    else
+        echo -e "$G Installing $2 is SUCCESS $N"   
+    fi  
+}
+
+
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
 
 
 if [ $USERID -ne 0 ]
@@ -15,7 +31,15 @@ fi
 # All argumnets are in $@        Package names given from commandline eg: sh 12-loops.sh mysql git
 for i in $@
 do
-    yum install $i -y
+    yum list installed git $i &>> $LOG_FILE
+    if [ $? -ne 0 ]
+    then
+        echo "$i is not installed, let's install it"
+        yum install $i -y &>> $LOG_FILE
+        VALIDATE $? "$i"
+    else
+        echo -e "$Y $i is already installed"
+    fi    
 done     
 
 #Improvements
