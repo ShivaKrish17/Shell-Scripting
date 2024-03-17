ca35a1005a19fd96910bcb81da7336b6e235cc79
unknown <shiva539k@gmail.com>
2024-03-17 09:00:38 +0530

    Variables through commandline script

---
+++ 03-variables.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+
+echo "Hello World"
+
+PERSON1=$1
+PERSON2=$2 
+
+echo "$PERSON1: Oh? Bob!" 
+
+echo "$PERSON2: Hey Sam! Good to see you!"
+
+echo "$PERSON1: How’s it going?" 
+
+echo "$PERSON2: Yeah, good. Working a lot. And you?"
+
+echo "$PERSON1: I went back to school."
+
+echo "$PERSON2: Good for you!"
+
+#In command line type, sh 03-variables.sh <variable1> <variable2>
