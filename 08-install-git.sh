3fc621e0b70bde729f1acd1bf2fe7e4c6bde28a7
unknown <shiva539k@gmail.com>
2024-03-17 12:51:23 +0530

    Validate Root User: GIT Installation Script

--- 08-install-git.sh
+++ 08-install-git.sh
@@ -10,6 +10,25 @@ then
     exit 15               # 1 - 127 is failure
 fi    
 
+
+#It is our responsibility to check if installation is success or not
+
 yum install git -y
 
+if [ $? -ne 0 ]
+then 
+    echo "Installation of Git is error"
+    exit 1
+else 
+    echo "Installation of Git is Success"    
+fi    
+
 yum install mysql -y
+
+if [ $? -ne 0 ]
+then 
+    echo "Installation of MySQL is error"
+    exit 1
+else
+    echo "Installation of MySQL is error"    
+fi  
